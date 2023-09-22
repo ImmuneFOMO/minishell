ed00754ecb8998a9ce244aa5a2f6c96ae31e638f
trelixon <rikiton.shift@gmail.com>
2023-09-22 04:48:54 +0100

    feat: new version with tests

--- file/minishell.c
+++ file/minishell.c
@@ -6,16 +6,40 @@
 /*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:54:21 by azhadan           #+#    #+#             */
-/*   Updated: 2023/09/21 19:16:24 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/09/22 03:25:05 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../h_files/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+int	main()
 {
-	(void)argc;
-	(void)argv;
-	(void)envp;
+	char *input;
+	pid_t pid;
+	int status;
+
+	while (1) {
+		input = readline("minishell> ");
+		if (!input)
+			break ;
+	
+	add_history(input);
+	pid = fork();
+	if (pid == 0)
+	{
+		char *args[] = {"/bin/sh", "-c", input, NULL};
+		execve(args[0], args, NULL);
+		perror("execve");
+		exit (1);
+	}
+	else if (pid < 0)
+		perror("fork");
+	else
+	{
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+			waitpid(pid, &status, WUNTRACED);
+	}
+	free(input);
+	}
 	return (0);
 }
--- h_files/minishell.h
+++ h_files/minishell.h
@@ -6,43 +6,27 @@
 /*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:26:57 by azhadan           #+#    #+#             */
-/*   Updated: 2023/09/21 22:24:56 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/09/21 23:39:49 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/libft.h"
-/* libft - ft_atoi, ft_bzero, ft_calloc, ft_isalnum, ft_isalpha, ft_isascii,
-	ft_isdigit, ft_isprint, ft_itoa, ft_lstadd_back, ft_lstadd_front,
-	ft_lstclear, ft_lstdelone, ft_lstiter, ft_lstlast, ft_lstmap, ft_lstnew,
-	ft_lstsize, ft_memchr, ft_memcmp, ft_memcpy, ft_memmove, ft_memset,
-	ft_printf, ft_putchar_fd, ft_putendl_fd, ft_putnbr_fd, ft_putstr_fd,
-	ft_source, ft_split, ft_strchr, ft_strdup, ft_striteri,
-	ft_strjoin, ft_strlcat, ft_strlcpy, ft_strlen, ft_strmapi, ft_strncmp,
-	ft_strnstr, ft_strrchr, ft_strtrim, ft_substr, ft_tolower, ft_toupper,
-	get_next_line */
-# include <stdlib.h>
-// stdlib.h - malloc, free, exit, getenv
-# include <sys/wait.h>
-// sys/wait.h - wait, waitpid, wait3, wait4
-# include <unistd.h>
-// unistd.h - write, access, open, read, close, fork, signal, sigaction, kill,
-// getcwd, chdir, unlink, execve, dup, dup2, pipe, isatty, ttyname, ioctl,
-//	tcsetattr, tcgetattr
-# include <signal.h>
-// signal.h - sigemptyset, sigaddset
-# include <sys/stat.h>
-# include <sys/types.h>
-// sys/types.h, sys/stat.h - stat, lstat, fstat, opendir, readdir, closedir
-# include <string.h>
-// string.h - strerror, perror
-# include <readline/history.h>
-# include <readline/readline.h>
-// readline/readline.h, readline/history.h - readline, rl_clear_history,
-//	rl_on_new_line,
-// rl_replace_line, rl_redisplay, add_history
+#include <stdlib.h> // malloc, free, exit, getenv
+#include <stdio.h>  // printf
+#include <sys/wait.h> // wait, waitpid, wait3, wait4
+#include <unistd.h> /* write, access, open, read, close, fork, signal, sigaction, kill,
+                     getcwd, chdir, unlink, execve, dup, dup2, pipe, isatty, ttyname, ioctl,
+                     tcsetattr, tcgetattr, ttyslot (verify for your system) */
+#include <signal.h> // sigemptyset, sigaddset
+#include <sys/stat.h>
+#include <sys/types.h> // sys/types.h, sys/stat.h - stat, lstat, fstat, opendir, readdir, closedir
+#include <string.h> // strerror, perror
+#include <readline/history.h>
+#include <readline/readline.h> /* readline, rl_clear_history, rl_on_new_line,
+                               rl_replace_line, rl_redisplay, add_history */
+#include <term.h> // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
 
 
 #endif
