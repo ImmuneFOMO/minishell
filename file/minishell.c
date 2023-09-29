5713d579f9fb1281984aaf4b7107d0d1dd767d64
ImmuneFOMO <rikiton.shift@gmail.com>
2023-09-29 04:22:25 +0000

    fix: problem with empty line and collect it in history

--- file/minishell.c
+++ file/minishell.c
@@ -6,7 +6,7 @@
 /*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:54:21 by azhadan           #+#    #+#             */
-/*   Updated: 2023/09/29 02:57:39 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/09/29 04:21:35 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ int	main(int argc, char **argv, char **envp)
 {
 	char *input;
 
+	(void)argc;
+	(void)argv;
+	(void)envp;
 	while ((input = readline("minishell> ")) != NULL)
 	{
+		if (!input[0])
+			continue;
 		add_history(input);
 		if (ft_strncmp(input, "exit", 4) == 0)
 		{
