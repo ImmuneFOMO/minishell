2d1146ff04f3cb0f2b32cbaf04b69baf71387e18
ImmuneFOMO <rikiton.shift@gmail.com>
2023-12-22 01:52:00 +0000

    refactor: norminette

--- c_files/builtins_second.c
+++ c_files/builtins_second.c
@@ -6,184 +6,201 @@
 /*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 11:37:12 by azhadan           #+#    #+#             */
-/*   Updated: 2023/12/21 22:34:05 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/12/22 01:51:04 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../h_files/minishell.h"
 
-int is_valid_var_name(char *var_name)
+int	is_valid_var_name(char *var_name)
 {
-    if (!var_name || !((var_name[0] >= 'a' && var_name[0] <= 'z') || (var_name[0] >= 'A' && var_name[0] <= 'Z'))) {
-        return 0;
-    }
-
-    for (int i = 1; var_name[i]; i++) {
-        if (!((var_name[i] >= '0' && var_name[i] <= '9') || (var_name[i] >= 'a' && var_name[i] <= 'z') || (var_name[i] >= 'A' && var_name[i] <= 'Z') || var_name[i] == '_')) {
-            return 0;
-        }
-    }
-    return 1;
+	if (!var_name || !((var_name[0] >= 'a' && var_name[0] <= 'z')
+			|| (var_name[0] >= 'A' && var_name[0] <= 'Z')))
+	{
+		return (0);
+	}
+	for (int i = 1; var_name[i]; i++)
+	{
+		if (!((var_name[i] >= '0' && var_name[i] <= '9') || (var_name[i] >= 'a'
+					&& var_name[i] <= 'z') || (var_name[i] >= 'A'
+					&& var_name[i] <= 'Z') || var_name[i] == '_'))
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
 
-int builtin_export_name_valid(int var_name_length, char **var, char ***var_val)
+int	builtin_export_name_valid(int var_name_length, char **var, char ***var_val)
 {
-    char var_name[var_name_length + 1];
-    
-    ft_strncpy(var_name, (*var), var_name_length);
-    var_name[var_name_length] = '\0';
-    if (!is_valid_var_name(var_name)) 
-    {
-        g_exit_code = 1;
-        ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
-        return (1);
-    }
-    (*var_val) = ft_split((*var), '=');
-    if ((*var_val) == NULL || (*var_val)[0] == NULL)
-    {
-        ft_free_strs((*var_val));
-        return (2);
-    }
-    return (0);
+	char	var_name[var_name_length + 1];
+
+	ft_strncpy(var_name, (*var), var_name_length);
+	var_name[var_name_length] = '\0';
+	if (!is_valid_var_name(var_name))
+	{
+		g_exit_code = 1;
+		ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
+		return (1);
+	}
+	(*var_val) = ft_split((*var), '=');
+	if ((*var_val) == NULL || (*var_val)[0] == NULL)
+	{
+		ft_free_strs((*var_val));
+		return (2);
+	}
+	return (0);
 }
 
-int check_for_equal_sign(char *var)
+int	check_for_equal_sign(char *var)
 {
-    char *eq_pos = ft_strchr(var, '=');
-    if (eq_pos == NULL) 
-    {
-        if (!is_valid_var_name(var)) 
-        {
-            g_exit_code = 1;
-            ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
-            return (1);
-        }
-        return (0);
-    }
-    return (2);
+	char	*eq_pos;
+
+	eq_pos = ft_strchr(var, '=');
+	if (eq_pos == NULL)
+	{
+		if (!is_valid_var_name(var))
+		{
+			g_exit_code = 1;
+			ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
+			return (1);
+		}
+		return (0);
+	}
+	return (2);
 }
 
-int update_or_add_env_var(char ***envp, char ***var_val)
+int	update_or_add_env_var(char ***envp, char ***var_val)
 {
-    int j = 0;
+	int		j;
+	char	**env_var_val;
 
-    while ((*envp)[j]) {
-        char **env_var_val = ft_split((*envp)[j], '=');
-        if (!env_var_val) {
-            ft_free_strs((*var_val));
-            return (0);
-        }
-        if (env_var_val[0] && ft_strcmp(env_var_val[0], (*var_val)[0]) == 0) {
-            return update_environment_variable(envp, var_val, j, &env_var_val);
-        }
-        ft_free_strs(env_var_val);
-        j++;
-    }
-
-    return add_new_environment_variable(envp, var_val, j);
+	j = 0;
+	while ((*envp)[j])
+	{
+		env_var_val = ft_split((*envp)[j], '=');
+		if (!env_var_val)
+		{
+			ft_free_strs((*var_val));
+			return (0);
+		}
+		if (env_var_val[0] && ft_strcmp(env_var_val[0], (*var_val)[0]) == 0)
+		{
+			return (update_environment_variable(envp, var_val, j,
+					&env_var_val));
+		}
+		ft_free_strs(env_var_val);
+		j++;
+	}
+	return (add_new_environment_variable(envp, var_val, j));
 }
 
-int update_environment_variable(char ***envp, char ***var_val, int index, char ***env_var_val)
+int	update_environment_variable(char ***envp, char ***var_val, int index,
+		char ***env_var_val)
 {
-    char *temp; 
-    char *new_val;
-    
-    free((*envp)[index]);
-    temp = ft_strjoin((*var_val)[0], "=");
-    new_val = (*var_val)[1] ? ft_strjoin(temp, (*var_val)[1]) : ft_strjoin(temp, "");
-    free(temp);
-    if (!new_val) 
-    {
-        ft_free_strs((*env_var_val));
-        ft_free_strs((*var_val));
-        return (0);
-    }
-    (*envp)[index] = ft_strdup(new_val);
-    free(new_val);
-    if (!(*envp)[index]) 
-    {
-        ft_free_strs((*env_var_val));
-        ft_free_strs((*var_val));
-        return (0);
-    }
-    ft_free_strs((*env_var_val));
-    ft_free_strs((*var_val));
-    return (0);
+	char	*temp;
+	char	*new_val;
+
+	free((*envp)[index]);
+	temp = ft_strjoin((*var_val)[0], "=");
+	new_val = (*var_val)[1] ? ft_strjoin(temp, (*var_val)[1]) : ft_strjoin(temp,
+			"");
+	free(temp);
+	if (!new_val)
+	{
+		ft_free_strs((*env_var_val));
+		ft_free_strs((*var_val));
+		return (0);
+	}
+	(*envp)[index] = ft_strdup(new_val);
+	free(new_val);
+	if (!(*envp)[index])
+	{
+		ft_free_strs((*env_var_val));
+		ft_free_strs((*var_val));
+		return (0);
+	}
+	ft_free_strs((*env_var_val));
+	ft_free_strs((*var_val));
+	return (0);
 }
 
-int add_new_environment_variable(char ***envp, char ***var_val, int size)
+int	add_new_environment_variable(char ***envp, char ***var_val, int size)
 {
-    char *temp;
-    char *new_val;
-    char **new_envp = malloc((size + 2) * sizeof(char *));
-    if (!new_envp) 
-    {
-        ft_free_strs((*var_val));
-        return (0);
-    }
-    for (int i = 0; i < size; i++) {
-        new_envp[i] = ft_strdup((*envp)[i]);
-    }
-    temp = ft_strjoin((*var_val)[0], "=");
-    new_val = (*var_val)[1] ? ft_strjoin(temp, (*var_val)[1]) : ft_strjoin(temp, "");
-    free(temp);
+	char	*temp;
+	char	*new_val;
+	char	**new_envp;
 
-    if (!new_val) {
-        free(new_envp);
-        ft_free_strs((*var_val));
-        return (1);
-    }
-
-    new_envp[size] = ft_strdup(new_val);
-    free(new_val);
-    if (!new_envp[size]) {
-        free(new_envp);
-        ft_free_strs((*var_val));
-        return (1);
-    }
-
-    new_envp[size + 1] = NULL;
-    ft_free_strs((*envp));
-    (*envp) = new_envp;
-    ft_free_strs((*var_val));
-    return (0);
+	new_envp = malloc((size + 2) * sizeof(char *));
+	if (!new_envp)
+	{
+		ft_free_strs((*var_val));
+		return (0);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		new_envp[i] = ft_strdup((*envp)[i]);
+	}
+	temp = ft_strjoin((*var_val)[0], "=");
+	new_val = (*var_val)[1] ? ft_strjoin(temp, (*var_val)[1]) : ft_strjoin(temp,
+			"");
+	free(temp);
+	if (!new_val)
+	{
+		free(new_envp);
+		ft_free_strs((*var_val));
+		return (1);
+	}
+	new_envp[size] = ft_strdup(new_val);
+	free(new_val);
+	if (!new_envp[size])
+	{
+		free(new_envp);
+		ft_free_strs((*var_val));
+		return (1);
+	}
+	new_envp[size + 1] = NULL;
+	ft_free_strs((*envp));
+	(*envp) = new_envp;
+	ft_free_strs((*var_val));
+	return (0);
 }
 
-int builtin_export(char *var, char ***envp)
+int	builtin_export(char *var, char ***envp)
 {
-    char    **var_val;
-    int     var_name_length;
-    char    *eq_pos;
+	char	**var_val;
+	int		var_name_length;
+	char	*eq_pos;
+	int		j;
 
-    eq_pos = ft_strchr(var, '=');
-    if (eq_pos == NULL)
-    {
-        if (!is_valid_var_name(var)) 
-        {
-        g_exit_code = 1;
-        ft_printf("%s", var);
-        ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
-        return (1);
-        }
-    return (0);
-    }
-    var_name_length = eq_pos - var;
-    int j = builtin_export_name_valid(var_name_length, &var, &var_val);
-    if (j != 0)
-        return j;
-
-    return update_or_add_env_var(envp, &var_val);
+	eq_pos = ft_strchr(var, '=');
+	if (eq_pos == NULL)
+	{
+		if (!is_valid_var_name(var))
+		{
+			g_exit_code = 1;
+			ft_printf("%s", var);
+			ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
+			return (1);
+		}
+		return (0);
+	}
+	var_name_length = eq_pos - var;
+	j = builtin_export_name_valid(var_name_length, &var, &var_val);
+	if (j != 0)
+		return (j);
+	return (update_or_add_env_var(envp, &var_val));
 }
 
-void ft_free_strs(char **strs)
+void	ft_free_strs(char **strs)
 {
-    int i;
-    
-    i = 0;
+	int	i;
+
+	i = 0;
 	while (strs[i])
 	{
-	free(strs[i]);
-	i++;
+		free(strs[i]);
+		i++;
 	}
 	free(strs);
 }
--- c_files/cmnds1.c
+++ c_files/cmnds1.c
@@ -6,61 +6,63 @@
 /*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 23:30:46 by idlbltv           #+#    #+#             */
-/*   Updated: 2023/12/21 03:05:56 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/12/22 01:48:50 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../h_files/minishell.h"
 
-int handle_heredoc(struct s_redircmd *rcmd)
+int	handle_heredoc(struct s_redircmd *rcmd)
 {
-    char buffer[1024];
-    int pipefd[2];
-    size_t delimiter_length;
-    ssize_t read_len;
+	char	buffer[1024];
+	int		pipefd[2];
+	size_t	delimiter_length;
+	ssize_t	read_len;
 
-    if (rcmd == NULL || rcmd->file == NULL)
-    {
-        write(STDERR_FILENO, "Invalid command\n", 16);
-        return 0;
-    }
-    if (pipe(pipefd) == -1)
-    {
-        perror("pipe");
-        return 0;
-    }
-    delimiter_length = ft_strlen(rcmd->file);
-    while (1)
-    {
-        write(STDERR_FILENO, ">", 1);
-        read_len = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
-        if (read_len <= 0)
-            break;
-        buffer[read_len] = '\0';
-        if (ft_strncmp(buffer, rcmd->file, delimiter_length) == 0 &&
-            (buffer[delimiter_length] == '\n' || buffer[delimiter_length] == '\0'))
-            break;
-        write(pipefd[1], buffer, read_len);
-    }
-    close(pipefd[1]);
-    if (dup2(pipefd[0], STDIN_FILENO) == -1)
-    {
-        perror("dup2");
-        close(pipefd[0]);
-        return 0;
-    }
-    close(pipefd[0]);
-    return 1;
+	if (rcmd == NULL || rcmd->file == NULL)
+	{
+		write(STDERR_FILENO, "Invalid command\n", 16);
+		return (0);
+	}
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe");
+		return (0);
+	}
+	delimiter_length = ft_strlen(rcmd->file);
+	while (1)
+	{
+		write(STDERR_FILENO, ">", 1);
+		read_len = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+		if (read_len <= 0)
+			break ;
+		buffer[read_len] = '\0';
+		if (ft_strncmp(buffer, rcmd->file, delimiter_length) == 0
+			&& (buffer[delimiter_length] == '\n'
+				|| buffer[delimiter_length] == '\0'))
+			break ;
+		write(pipefd[1], buffer, read_len);
+	}
+	close(pipefd[1]);
+	if (dup2(pipefd[0], STDIN_FILENO) == -1)
+	{
+		perror("dup2");
+		close(pipefd[0]);
+		return (0);
+	}
+	close(pipefd[0]);
+	return (1);
 }
 
 int	runcmd(struct s_cmd *cmd)
 {
 	char	type;
-    int     exit_code = 0;
+	int		exit_code;
 
+	exit_code = 0;
 	type = cmd->type;
 	if (cmd == 0)
-		return(0);
+		return (0);
 	if (type == ' ')
 		exit_code = execute_command(cmd);
 	else if (type == '>' || type == '<' || type == '+' || type == '%')
@@ -80,53 +82,59 @@ int	runcmd(struct s_cmd *cmd)
 	return (exit_code);
 }
 
-int check_error(char *cmd) {
-    if (errno == EACCES) {
-        write(2, cmd, strlen(cmd));
-        write(2, ": permission denied\n", 20);
-        return 126;
-    } else if (errno == ENOENT) {
-        write(2, cmd, strlen(cmd));
-        write(2, ": command not found\n", 20);
-        return 127;
-    } else {
-        perror(cmd);
-        return 127;
-    }
+int	check_error(char *cmd)
+{
+	if (errno == EACCES)
+	{
+		write(2, cmd, strlen(cmd));
+		write(2, ": permission denied\n", 20);
+		return (126);
+	}
+	else if (errno == ENOENT)
+	{
+		write(2, cmd, strlen(cmd));
+		write(2, ": command not found\n", 20);
+		return (127);
+	}
+	else
+	{
+		perror(cmd);
+		return (127);
+	}
 }
 
 int	execute_command(struct s_cmd *cmd)
 {
-    struct s_execcmd	*ecmd;
-    char				*full_path;
+	struct s_execcmd	*ecmd;
+	char				*full_path;
 
-    ecmd = (struct s_execcmd *)cmd;
-    if (ecmd->argv[0] == 0)
-        return(0);
-    if (ft_strncmp(ecmd->argv[0], "cd", 3) == 0 || 
-    ft_strncmp(ecmd->argv[0], "export", 7) == 0 || 
-    ft_strncmp(ecmd->argv[0], "unset", 6) == 0)
-        return (g_exit_code);
-    if (builtins(ecmd))
-        return (g_exit_code);
-    full_path = find_in_path(ecmd->argv[0]);
-    if (full_path)
-    {
-        execve(full_path, ecmd->argv, ecmd->envp);
-        if (errno)
-            g_exit_code = check_error(ecmd->argv[0]);
-        free(full_path);
-    }
-    else
-    {
-        if (ft_strncmp(ecmd->argv[0], "cd", 3) != 0 && ft_strncmp(ecmd->argv[0], "export", 7) != 0 && ft_strncmp(ecmd->argv[0], "unset", 6) != 0)
-        {
-            execve(ecmd->argv[0], ecmd->argv, ecmd->envp);
-            if (errno)
-                g_exit_code = check_error(ecmd->argv[0]);    
-        }
-    }
-    return (g_exit_code);
+	ecmd = (struct s_execcmd *)cmd;
+	if (ecmd->argv[0] == 0)
+		return (0);
+	if (ft_strncmp(ecmd->argv[0], "cd", 3) == 0 || ft_strncmp(ecmd->argv[0],
+			"export", 7) == 0 || ft_strncmp(ecmd->argv[0], "unset", 6) == 0)
+		return (g_exit_code);
+	if (builtins(ecmd))
+		return (g_exit_code);
+	full_path = find_in_path(ecmd->argv[0]);
+	if (full_path)
+	{
+		execve(full_path, ecmd->argv, ecmd->envp);
+		if (errno)
+			g_exit_code = check_error(ecmd->argv[0]);
+		free(full_path);
+	}
+	else
+	{
+		if (ft_strncmp(ecmd->argv[0], "cd", 3) != 0 && ft_strncmp(ecmd->argv[0],
+				"export", 7) != 0 && ft_strncmp(ecmd->argv[0], "unset", 6) != 0)
+		{
+			execve(ecmd->argv[0], ecmd->argv, ecmd->envp);
+			if (errno)
+				g_exit_code = check_error(ecmd->argv[0]);
+		}
+	}
+	return (g_exit_code);
 }
 
 void	redirect_command(struct s_redircmd *rcmd)
@@ -142,25 +150,25 @@ void	redirect_command(struct s_redircmd *rcmd)
 	{
 		fd_redirect = handle_heredoc(rcmd);
 		if (fd_redirect == 0)
-  		{
-   		perror("double_redirect_left");
-   		return ;
- 		}
- 		runcmd(rcmd->cmd);
-  		return ;
+		{
+			perror("double_redirect_left");
+			return ;
+		}
+		runcmd(rcmd->cmd);
+		return ;
 	}
 	fd_redirect = open(rcmd->file, flags, 0666);
 	if (fd_redirect < 0)
 	{
 		perror("open");
-        close(fd_redirect);
+		close(fd_redirect);
 		return ;
 	}
 	if (dup2(fd_redirect, rcmd->fd) < 0)
 	{
 		perror("dup2");
 		close(fd_redirect);
-        return ;
+		return ;
 	}
 	rcmd->cmd->envp = dup_envp(rcmd->envp);
 	runcmd(rcmd->cmd);
