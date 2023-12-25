422dbe3497c7cdfde654a5089285fa934eab251e
dlbltv <idlbltv@gmail.com>
2023-12-25 17:06:07 +0000

    fix:parsepipe function. Add error for empty cmnd ||

--- c_files/parsing_comands.c
+++ c_files/parsing_comands.c
@@ -6,22 +6,22 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 23:51:46 by azhadan           #+#    #+#             */
-/*   Updated: 2023/12/24 21:02:50 by root             ###   ########.fr       */
+/*   Updated: 2023/12/25 17:04:20 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../h_files/minishell.h"
 
-void	handle_error(const char *error_message)
+void handle_error(const char *error_message)
 {
 	write(2, error_message, ft_strlen(error_message));
 	exit(2);
 }
 
-struct s_cmd	*parsecmd(char *s, char **envp)
+struct s_cmd *parsecmd(char *s, char **envp)
 {
-	char			*es;
-	struct s_cmd	*cmd;
+	char *es;
+	struct s_cmd *cmd;
 
 	es = s + ft_strlen(s);
 	cmd = parseline(&s, es, envp);
@@ -31,18 +31,18 @@ struct s_cmd	*parsecmd(char *s, char **envp)
 	return (cmd);
 }
 
-struct s_cmd	*parseline(char **ps, char *es, char **envp)
+struct s_cmd *parseline(char **ps, char *es, char **envp)
 {
-	struct s_cmd	*cmd;
+	struct s_cmd *cmd;
 
 	cmd = parsesemicolon(ps, es, envp);
 	return (cmd);
 }
 
-struct s_cmd	*parsesemicolon(char **ps, char *es, char **envp)
+struct s_cmd *parsesemicolon(char **ps, char *es, char **envp)
 {
-	struct s_cmd	*cmd;
-	int				tok;
+	struct s_cmd *cmd;
+	int tok;
 
 	cmd = parsepipe(ps, es, envp);
 	if (peek(ps, es, ";"))
@@ -55,28 +55,30 @@ struct s_cmd	*parsesemicolon(char **ps, char *es, char **envp)
 	return (cmd);
 }
 
-struct s_cmd	*parsepipe(char **ps, char *es, char **envp)
+struct s_cmd *parsepipe(char **ps, char *es, char **envp)
 {
-	struct s_cmd	*cmd;
-	char			*prev;
+	struct s_cmd *cmd;
+	char *prev;
+
+	prev = NULL;
 
 	cmd = parseexec(ps, es, envp);
 	while (peek(ps, es, "|"))
 	{
 		prev = *ps;
 		gettoken(ps, es, 0, 0);
-		if (peek(ps, es, "|") || *ps == es)
+		while (peek(ps, es, "|"))
 		{
 			if (*ps != prev + 1)
 				handle_error("syntax error near unexpected token `|'\n");
 			else
 			{
 				gettoken(ps, es, 0, 0);
-				if (peek(ps, es, "|")) 
+				if (peek(ps, es, "|"))
 					handle_error("syntax error near unexpected token `|'\n");
 			}
 		}
-		else if (*ps == es)
+		if (*ps == es)
 			handle_error("syntax error near unexpected token `|'\n");
 		cmd = pipecmd(cmd, parseexec(ps, es, envp));
 	}
